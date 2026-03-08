20025c17f6cabe6cbd26d06ad1242f0994c344fe
jolie2000 <ujolie735@gmail.com>
2026-03-08 20:42:27 +0300

    deploying variables

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'karibu-grocerie.netlify.app' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
